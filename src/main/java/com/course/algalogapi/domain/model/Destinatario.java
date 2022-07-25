ff171904b339fcbae6d0ff3695fc4f9942aace80
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-25 16:59:52 -0300

    Destinatario model Embeddable

---
+++ src/main/java/com/course/algalogapi/domain/model/Destinatario.java
@@ -0,0 +1,50 @@
+package com.course.algalogapi.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Destinatario {
+
+	@Column(name = "destinatario_logradouro")
+	private String logradouro;
+	@Column(name = "destinatario_nome")
+	private String nome;
+	@Column(name = "destinatario_complemento")
+	private String complemento;
+	@Column(name = "destinatario_bairro")
+	private String bairro;
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+}
