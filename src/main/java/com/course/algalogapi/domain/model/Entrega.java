3a4036f1d2aec6d6becaec45b0695c55a21d6454
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-25 16:57:45 -0300

    Entrega model e relação Many-to-One

---
+++ src/main/java/com/course/algalogapi/domain/model/Entrega.java
@@ -0,0 +1,95 @@
+package com.course.algalogapi.domain.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_entrega")
+public class Entrega implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	private Cliente cliente;
+
+	@Embedded
+	private Destinatario destinatario;
+	private BigDecimal taxa;
+
+	@Enumerated(EnumType.STRING)
+	private StatusEntrega status;
+	private LocalDateTime datapedido;
+
+	private LocalDateTime dataFinalizacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Destinatario getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(Destinatario destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public BigDecimal getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(BigDecimal taxa) {
+		this.taxa = taxa;
+	}
+
+	public StatusEntrega getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEntrega status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDatapedido() {
+		return datapedido;
+	}
+
+	public void setDatapedido(LocalDateTime datapedido) {
+		this.datapedido = datapedido;
+	}
+
+	public LocalDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+
+	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+
+}
