10370178ef57e668263be05c04d21eb3a01bf726
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-25 17:07:48 -0300

    Entrega repository

---
+++ src/main/java/com/course/algalogapi/repository/EntregaRepository.java
@@ -0,0 +1,9 @@
+package com.course.algalogapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.algalogapi.domain.model.Entrega;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Long> {
+
+}
